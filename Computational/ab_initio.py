f497c84ff642f6039a16024b72e8b3fec3421f10
jakub <jakub.garwola@gmail.com>
2022-11-09 15:15:14 +0100

    plotting energy

--- Computational/ab_initio.py
+++ Computational/ab_initio.py
@@ -1,23 +1,21 @@
 m = 1.0 #mass
-nst = 10000 #number of spets
+nst = 100 #number of spets
 dt = 0.0001 #time step size
 k = 1.0 #Boltzman constant
 T = 1.0 #initial temperature
 N = 3 #number of partickles
-L = 10 #size of the boxx
+L = 10 #size of the box
+epsilon = 1.0 #parameter for LJ potencial
+sigma = k*T/m
 
 import numpy as np
 from matplotlib import pyplot as plt
 
-sigma = k*T/m
-
 def modL(r):
     return r%L
 
-R_0 = np.array([np.random.normal(0.0,1.0),np.random.normal(0.0,sigma),0])
-for i in range(N-1):
-    R_0 = np.vstack((R_0,[[np.random.normal(0.0,1.0),
-                        np.random.normal(0.0,sigma),0]]))
+def dist(R,i,j):
+    return R[i,0]-R[j,0]
 
 def temp(R, i, eta = 1, N = 1):
     Ekin = 0.0
@@ -27,10 +25,21 @@ def temp(R, i, eta = 1, N = 1):
 
 def force(R,i,j):
     sigma = 1.0
-    epsilon = 1.0
-    dist = R[i,0]-R[j,0]
-    return -4*epsilon*( 12*((sigma/dist)**12) - 6*((sigma/dist)**6) )/dist
+    d = dist(R,i,j)
+    return -4*epsilon*( 12*((sigma/d)**12) - 6*((sigma/d)**6) )/d
     #return 1/dist**2
+
+def calc_energy(R):
+    Epot = np.empty(N)
+    Ekin = np.empty(N)
+    for i in range(N-1):
+        Epot[i] = 0.0
+        for j in range(N-1):
+            if(i != j):
+                d = dist(R_0,i,j)
+                Epot[i] += -4*epsilon*( ((sigma/d)**12) - ((sigma/d)**6) )
+        Ekin[i] = m*R_0[i,1]**2/2
+    return np.reshape(np.array([Ekin,Epot]),(3,2))
 
 def step(R):
     Rp = np.zeros(np.shape(R_0))
@@ -45,20 +54,25 @@ def step(R):
         Rp[i] = np.array([modL(r),v,F])
     return Rp
 
-# data([step, partickle, variable])
-data = np.empty((nst,N,3))
+R_0 = np.array([np.random.normal(0.0,1.0),np.random.normal(0.0,sigma),0])
+for i in range(N-1):
+    R_0 = np.vstack((R_0,[[np.random.normal(0.0,1.0),
+                        np.random.normal(0.0,sigma),0]]))
+
+data = np.empty((nst,N,3)) # data([step, partickle, variable])
 data[0] = R_0
-#etot = np.array([])
+ens = np.empty((nst,N,2)) # energies([step, partickle, [kin,pot] ])
+ens[0] = calc_energy(R_0) # check if reshape makes sense
 
 for i in range(1,nst): #1 not to use random bites form empty
     input = data[i-1]
     output = step(input)
     data[i] = output
-    #etot = np.append(etot, input[0]**2/2 + m/2*input[1]**2)
+    ens[i] = calc_energy(output)
 
 np.savetxt("data.txt", np.reshape(data,(nst*N,3)))
 
 fig,ax = plt.subplots(2)
 ax[0].plot(range(nst),data[:,:,0])
-#ax[1].plot(range(nst),etot)
+ax[1].plot(range(nst),ens[:,:,1])
 plt.show()
